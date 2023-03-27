5b19311a2747a3fb36e2e3bd44355aad70120c0d
BlackBurntTurkeyLeg <128901325+BlackBurntTurkeyLeg@users.noreply.github.com>
2023-03-27 17:24:37 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,26 @@
-window.open("https://google.com")
+You can do something like this:
+
+var theWindow = window.open('https://www.google.com'),
+    theDoc = theWindow.document,
+    theScript = document.createElement('script');
+function injectThis() {
+    // The code you want to inject goes here
+    alert(document.body.innerHTML);
+}
+theScript.innerHTML = 'window.onload = ' + injectThis.toString() + ';';
+theDoc.body.appendChild(theScript);
+This also seems to work:
+
+var theWindow = window.open('http://stackoverflow.com'),
+    theScript = document.createElement('script');
+function injectThis() {
+    // The code you want to inject goes here
+    alert(document.body.innerHTML);
+}
+// Self executing function
+theScript.innerHTML = '(' + injectThis.toString() + '());';
+theWindow.onload = function () {
+    // Append the script to the new window's body.
+    // Only seems to work with `this`
+    this.document.body.appendChild(theScript);
+};
