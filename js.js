2f0e1f2258a8cdf8f32303669c8785d49fe57fef
BlackBurntTurkeyLeg <128901325+BlackBurntTurkeyLeg@users.noreply.github.com>
2023-03-27 17:18:18 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,9 +1,1 @@
-var theWindow = window.open('about:blank'),
-    theDoc = theWindow.document,
-    theScript = document.createElement('script');
-function injectThis() {
-    // The code you want to inject goes here
-    alert("A");
-}
-theScript.innerHTML = 'window.onload = ' + injectThis.toString() + ';';
-theDoc.body.appendChild(theScript);
+
