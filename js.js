b975c0f751b77c359c13d42a374f6899f677a5e7
BlackBurntTurkeyLeg <128901325+BlackBurntTurkeyLeg@users.noreply.github.com>
2023-03-27 17:06:33 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,9 @@
 var theWindow = window.open('http://stackoverflow.com'),
+    theDoc = theWindow.document,
     theScript = document.createElement('script');
 function injectThis() {
     // The code you want to inject goes here
     alert(document.body.innerHTML);
 }
-// Self executing function
-theScript.innerHTML = '(' + injectThis.toString() + '());';
-theWindow.onload = function () {
-    // Append the script to the new window's body.
-    // Only seems to work with `this`
-    this.document.body.appendChild(theScript);
-};
+theScript.innerHTML = 'window.onload = ' + injectThis.toString() + ';';
+theDoc.body.appendChild(theScript);
