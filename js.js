f539fb47fb3a16f360011273216cbcab5882267a
BlackBurntTurkeyLeg <128901325+BlackBurntTurkeyLeg@users.noreply.github.com>
2023-03-29 15:57:43 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var win = window.open("about:blank")
 const doc = win.document
-doc.write(toString(window.document))
-
+doc.write(toString(window.document.documentElement))
+alert(toString(window.documentElement))
 window.close();
